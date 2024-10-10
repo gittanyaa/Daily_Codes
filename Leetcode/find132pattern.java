cbc3eff86fa55e0e590969cdcea592ee854fb83d
Tanya Tiwari <ta758nya@gmail.com>
2024-10-10 22:35:13 +0530

    find132pattern.java

---
+++ Leetcode/find132pattern.java
@@ -0,0 +1,24 @@
+package Leetcode;
+
+import java.util.Stack;
+
+public class find132pattern {
+   
+    public boolean find132patterns(int[] nums) {
+        Stack<Integer> stack = new Stack<>();
+
+        int third = Integer.MIN_VALUE;
+
+        for(int i=nums.length-1;i>=0;i--){
+            if(nums[i] < third){
+                return true;
+            }
+            while(!stack.isEmpty() && stack.peek() < nums[i]){
+                third = stack.pop();
+            }
+            stack.push(nums[i]);
+        }
+        return false;
+    }
+}
+
